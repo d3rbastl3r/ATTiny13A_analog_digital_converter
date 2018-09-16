4a7b75eb6f2e2919ac8ba0eb1f6db94fff4b1ed3
d3rbastl3r <igor.martens.83@gmail.com>
2018-09-16 20:54:27 +0200

    Add the masic / empty implementation

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/**
+ * A minimal example to use the ADC to read analog values
+ *
+ * @author Igor Martens
+ * @since 16.09.2018
+ */
+
+#define F_CPU 9600000
+
+#include <avr/io.h>
+
+void setup() {
+}
+
+int main(void) {
+    setup();
+
+    while(1) {
+    }
+
+    return 0;
+}
